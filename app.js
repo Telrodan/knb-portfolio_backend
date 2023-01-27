841b77275682db4ef0b9bf2ffa273eb37aeaf0a9
bence keresztes-nagy <telrodan@gmail.com>
2023-01-27 20:04:34 +0100

    wip

--- app.js
+++ app.js
@@ -23,17 +23,24 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use((req, res, next) => {
-  next();
-  res.setHeader('no-referrer');
-});
-
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use(cors());
 app.use('/api/v1/todo-list/list', todoListRouter);
 app.use('/api/v1/todo-list/task', todoTaskRouter);
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
 // app.all('*', (req, res, next) => {
 //   const err = new Error(`Can't find ${req.originalUrl} on the server`);
 //   err.status = 'fail';
