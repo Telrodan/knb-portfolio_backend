a761c3e0761d7e8eb30836cb1233e6a3157350d2
bence keresztes-nagy <telrodan@gmail.com>
2023-01-27 20:00:34 +0100

    wip

--- app.js
+++ app.js
@@ -30,10 +30,17 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use(cors());
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
   next();
-  res.setHeader('no-referrer');
 });
 app.use('/api/v1/todo-list/list', todoListRouter);
 app.use('/api/v1/todo-list/task', todoTaskRouter);
